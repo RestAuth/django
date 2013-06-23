8f0ffd58398082dd20dac18fcbe764d3d74506e0
Mathias Ertl <mati@fsinf.at>
2013-06-23 14:56:00 +0200

    add license, unicode literals, cleanup

--- DjangoRestAuth/backend.py
+++ DjangoRestAuth/backend.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of DjangoRestAuth (https://django.restauth.net).
+#
+# DjangoRestAuth is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DjangoRestAuth is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DJANGORestAuth.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
 import logging
 
 from django.contrib.auth import get_user_model
@@ -15,7 +34,6 @@ log = logging.getLogger(__name__)
 
 class RestAuthBackend(object):
     def authenticate(self, username=None, password=None):
-        print('authenticate(%s, %s)' % (username, password))
         if username is None or password is None:
             return None
 
